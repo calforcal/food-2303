d9f36c51ba3bdf36c0418e3fd4848ec8ef976c23
Michael Callahan <mcalla242@gmail.com>
2023-07-21 10:26:53 -0600

    Update food facaade spec

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -2,16 +2,27 @@ require "rails_helper"
 
 RSpec.describe FoodFacade do
   describe "Food" do
-    it "can return all foods based on a search" do
-      query = "sweeet potatoes"
-      foods = FoodFacade.new(query).food_finder
+    describe "#food_finder" do
+      it "can return top 10 foods based on a search" do
+        query = "sweet potatoes"
+        foods = FoodFacade.new(query).food_finder
 
-      foods.each do |food|
-        expect(food).to be_a Food
-        expect(food.code).to be_a String
-        expect(food.description).to be_a String
-        expect(food.brand).to be_a String
-        expect(food.ingredients).to be_a String
+        foods.each do |food|
+          expect(food).to be_a Food
+          expect(food.code).to be_a String
+          expect(food.description).to be_a String
+          expect(food.brand).to be_a String
+          expect(food.ingredients).to be_a String
+        end
+      end
+    end
+
+    describe "#total_results" do
+      it "can return the total number of search results" do
+        query = "sweet potatoes"
+        num = FoodFacade.new(query).total_results
+
+        expect(num).to be_an Integer
       end
     end
   end
