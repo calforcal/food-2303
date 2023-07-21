09cb73c6e59da98b83a1bc2e77a3fc5c657f667e
Michael Callahan <mcalla242@gmail.com>
2023-07-21 09:08:09 -0600

    Add specs and features for welcome index page

---
+++ spec/welcome/index_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "Home Page", type: :feature do
+  describe "happy paths" do
+    it "has a search form that redirects to foods page" do
+      visit root_path
+
+      fill_in(:q, with: "sweet potatoes")
+      click_on("Search")
+
+      expect(current_path).to eq(foods_path)
+    end
+  end
+end
